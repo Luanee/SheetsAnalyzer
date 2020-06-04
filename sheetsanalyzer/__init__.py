5d8e17d84c29c7dece1fd92356edea5d58d902e2
Luanee <62288132+Luanee@users.noreply.github.com>
2020-06-04 16:41:14 +0200

    Update __init__.py

--- sheetsanalyzer/__init__.py
+++ sheetsanalyzer/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3.7
 
 __author__ = "Pit Nahrstedt"
-__author_email = 'Luanee@web.de'
+__author_email = 'luanee@web.de'
 __description__ = "A application to extract cells from an enormous number of files"
 __name__ = "sheetsanalyzer"
 __version__ = "0.0.1"
